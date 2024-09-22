ccab28d32fe2af1775b3fbd11961020f86f95ee6
Abir Das <abirdas422@gmail.com>
2024-09-22 22:27:31 +0600

    show country using map

--- src/components/Countries/Countries.jsx
+++ src/components/Countries/Countries.jsx
@@ -1,4 +1,5 @@
 import { useState, useEffect } from "react";
+import Country from "../Country/Country";
 
 const Countries = () => {
   const [countries, setCountries] = useState([]);
@@ -7,7 +8,14 @@ const Countries = () => {
       .then((res) => res.json())
       .then((data) => setCountries(data));
   }, []);
-  return <p>Countries: {countries.length}</p>;
+  return (
+    <>
+      <p>Countries: {countries.length}</p>
+      {countries.map((country) => (
+        <Country key={country.cca2} country={country}></Country>
+      ))}
+    </>
+  );
 };
 
 export default Countries;
